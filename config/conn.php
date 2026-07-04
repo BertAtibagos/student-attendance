5fc346bee5789acae7a0c9d668bd77aabd3e4323
BertAtibagos <atibagos.robert2300@gmail.com>
2026-07-04 10:19:44 +0800

    Added disabled attribute

---
+++ config/conn.php
@@ -0,0 +1,13 @@
+<?php
+    // $servername = "localhost";
+    // $serverusername = "root";
+    // $serverpassword= "sacred";
+    // $serverdb="schoolportal_fcpc_edu_ph";
+    // $serverport="3307";
+
+    $servername = "46.21.150.116";
+    $serverusername = "schoolportal_fcpc_edu_ph_remote_user";
+    $serverpassword= '6$Sf&i3@8!Nx5GcKYbjP';
+    $serverdb="schoolportal_fcpc_edu_ph";
+    $serverport="3306";
+?>
