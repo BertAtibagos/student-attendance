b5d32c54cd3fb7907088c28214f06185ad08bc72
BertAtibagos <atibagos.robert2300@gmail.com>
2026-07-04 09:07:32 +0800

    Adjusted the dropdown to handle different object shape

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -1,9 +1,19 @@
-export function Dropdown(name,content,defaultValue = "Select an option") {
+export function Dropdown(name,content,label,options = {}) {
+
+    const {
+        defaultValue = "Select an option",
+        valueKey = "id",
+        labelKey = "name"
+    } = options;
+
     return `
-            <select data-name="${name}" class="dropdown">
-                <option value="" disabled selected>${defaultValue}</option>
-                ${content.map((s) => 
-                        `<option value="${s}">${s}</option>`
-                    ).join('')}
-            </select>`;
+            <div class="col-md-4">
+                <label class="form-label">${label}</label>
+                <select class="form-select dropdown" data-name="${name}">
+                    <option value="">${defaultValue}</option>
+                    ${content.map(item => `
+                        <option value="${item[valueKey]}">${item[labelKey]}</option>
+                    `).join("")}
+                </select>
+            </div>`;
 }
