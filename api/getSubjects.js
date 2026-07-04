fcaf289a825924c0ee6fe1c06d1da3f0a494000e
BertAtibagos <atibagos.robert2300@gmail.com>
2026-07-04 10:19:20 +0800

    Api for subjects

--- api/getSubjects.js
+++ api/getSubjects.js
@@ -1,1 +1,12 @@
-export async function getSubjects(){}
+export async function getSubjects(){
+    let res = [];
+
+    try{
+        const req = await fetch(`controller/subjects.php`);
+        res = await req.json();
+    }catch(error){
+        console.error("Error fetching semester data:", error);
+    }
+
+    return res;
+}
