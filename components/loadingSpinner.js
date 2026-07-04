98cf748b16db5b6b4463bed615623cc5f44de122
BertAtibagos <atibagos.robert2300@gmail.com>
2026-07-04 12:01:40 +0800

    Added loading spinner component

---
+++ components/loadingSpinner.js
@@ -0,0 +1,13 @@
+export function loadingComp(){
+    return(
+        `<tr class="loading-spinner hide">
+            <td colspan="4">
+                <div class="text-center">
+                    <div class="spinner-border " role="status">
+                        <span class="sr-only"></span>
+                    </div>
+                </div>
+            </td>
+        </tr>`
+    );
+}
