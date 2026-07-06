446c572de7fdd892d6da1ed496ff45875b984dab
BertAtibagos <atibagos.robert2300@gmail.com>
2026-07-06 15:56:35 +0800

    Added attendance api

--- api/getAttendance.js
+++ api/getAttendance.js
@@ -1,3 +1,12 @@
 export async function getAttendance(semester, subject) {
-    return
+
+    let res = [];
+    
+    try{ 
+        const req = await fetch();
+        res = await req.json();
+    }catch(error){
+        console.error("Error fetching attendance data:", error);
+    }
+    return res;
 }
