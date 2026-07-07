150b961038a3f8f69255b20193dbb0d3c4de93cb
BertAtibagos <atibagos.robert2300@gmail.com>
2026-07-07 16:19:53 +0800

    Added payload and url for the fetch

--- api/getAttendance.js
+++ api/getAttendance.js
@@ -3,7 +3,18 @@ export async function getAttendance(semester, subject) {
     let res = [];
     
     try{ 
-        const req = await fetch();
+
+        const params = new URLSearchParams({
+            semester: semester,
+            subject: subject
+        });
+
+        const req = await fetch(`controller/attendance.php`,{
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: params
+        });
+        
         res = await req.json();
     }catch(error){
         console.error("Error fetching attendance data:", error);
